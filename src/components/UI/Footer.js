d4c9bcba810373ccd677696e8cc520c820823284
Mayank Setia <mayanksetia@Mayanks-MacBook-Air.local>
2023-03-27 00:37:35 +0530

    Configure React app for deployment to GitHub Pages

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -1,11 +1,11 @@
 import classes from "./Footer.module.css";
 
 const Footer = () => {
-    return (
-        <div className={classes.footer}>
-            <p>Made with ❤️ by Mayank Setia</p>
-        </div>
-    );
+  return (
+    <div className={classes.footer}>
+      <p>Made with ❤️ by Tanvi </p>
+    </div>
+  );
 };
 
 export default Footer;
